6986697274054cfdf79f69ef2c4cd70923533ca8
pmudassir <pmuddasir@gmail.com>
2023-07-17 22:04:32 +0530

    form redesigned

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,36 +1,33 @@
 const Form = () => {
     return (
-        <div>
-            <h2 className="text-3xl font-bold mb-6">Booking Form</h2>
-            <p className="text-lg mb-4">Please fill in the details below:</p>
-
-            <form>
-                <div className="mb-4">
-                    <label className="block mb-1 text-gray-600">First Name</label>
-                    <input type="text" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" />
-                </div>
-
-                <div className="mb-4">
-                    <label className="block mb-1 text-gray-600">Last Name</label>
-                    <input type="text" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" />
-                </div>
-
-                <div className="mb-4">
-                    <label className="block mb-1 text-gray-600">Email</label>
-                    <input type="email" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" />
-                </div>
-
-                <div className="mb-4">
-                    <label className="block mb-1 text-gray-600">Phone Number</label>
-                    <input type="tel" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" />
-                </div>
-
-                <button type="submit" className="bg-blue-500 text-white rounded px-6 py-3 hover:bg-blue-600">
-                    Submit
-                </button>
-            </form>
-        </div>
-    )
-}
-
-export default Form
+      <div className="bg-green-950 py-8 px-10 rounded-lg -mt-20 ml-20">
+        <h2 className="text-3xl font-bold mb-6 text-white font-work text-center">Schedule an Appointment</h2>
+        <p className="text-lg mb-4 text-white">Skyrocket your Business Growth!</p>
+  
+        <form>
+          <div className="mb-6">
+            <input type="text" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" placeholder="First Name" required />
+          </div>
+  
+          <div className="mb-6">
+            <input type="text" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" placeholder="Last Name" required />
+          </div>
+  
+          <div className="mb-6">
+            <input type="email" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" placeholder="E-mail" required />
+          </div>
+  
+          <div className="mb-6">
+            <input type="tel" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:border-blue-500" placeholder="Phone Number" required />
+          </div>
+  
+          <button type="submit" className="bg-blue-500 text-white rounded px-10 py-3 hover:bg-blue-600">
+            Schedule Now
+          </button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Form;
+  
